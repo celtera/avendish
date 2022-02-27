8ccaa8ee4895e9de1d6497a45601bd571254d1b9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-02-27 10:57:30 +0100

    Max fix

--- include/avnd/binding/max/audio_processor.hpp
+++ include/avnd/binding/max/audio_processor.hpp
@@ -146,8 +146,8 @@ struct audio_processor
   {
     processor.process(
         implementation,
-        avnd::span{ins, std::size_t(numins)},
-        avnd::span{outs, std::size_t(numouts)},
+        avnd::span<double*>{ins, std::size_t(numins)},
+        avnd::span<double*>{outs, std::size_t(numouts)},
         sampleframes);
   }
 
