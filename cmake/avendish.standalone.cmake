07beb43db484341addc121ac18727258000c44bd
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-03-22 23:36:07 +0100

    [ui] Refactor a bit the detection of Qt & Nuklear

--- cmake/avendish.standalone.cmake
+++ cmake/avendish.standalone.cmake
@@ -1,8 +1,15 @@
 find_package(ossia QUIET)
-find_package(GLEW REQUIRED)
-find_package(glfw3 REQUIRED)
-find_package(OpenGL REQUIRED)
-
+if(NOT TARGET Qt5::Quick)
+  find_package(GLEW QUIET)
+  find_package(glfw3 QUIET)
+  find_package(OpenGL QUIET)
+  
+  if(NOT ((TARGET GLEW::GLEW) AND (TARGET glfw) AND (TARGET OpenGL::GL)))
+    function(avnd_make_standalone)
+    endfunction()
+    return()
+  endif()
+endif()
 
 add_library(Avendish_standalone_pch STATIC "${AVND_SOURCE_DIR}/src/dummy.cpp")
 
@@ -37,7 +44,8 @@ endif()
 
 avnd_common_setup("" "Avendish_standalone_pch")
 
-function(avnd_make_standalone_qt)
+if(TARGET Qt5::Quick)
+function(avnd_make_standalone)
   cmake_parse_arguments(AVND "" "TARGET;MAIN_FILE;MAIN_CLASS" "" ${ARGN})
 
   string(MAKE_C_IDENTIFIER "${AVND_MAIN_CLASS}" MAIN_OUT_FILE)
@@ -94,8 +102,7 @@ function(avnd_make_standalone_qt)
     "${AVND_SOURCE_DIR}/include/avnd/binding/standalone/oscquery_mapper.hpp"
   )
 endfunction()
-
-
+else()
 function(avnd_make_standalone)
   cmake_parse_arguments(AVND "" "TARGET;MAIN_FILE;MAIN_CLASS" "" ${ARGN})
 
@@ -156,3 +163,4 @@ function(avnd_make_standalone)
     "${AVND_SOURCE_DIR}/include/avnd/binding/ui/nuklear/nuklear_glfw_gl4.h"
   )
 endfunction()
+endif()
