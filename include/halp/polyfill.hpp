7816d1b517ef74ba5dd1ef7794c763114532f633
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-10-01 00:31:39 +0200

    [macos] Fix build due to apple lying on version numbers...

--- include/halp/polyfill.hpp
+++ include/halp/polyfill.hpp
@@ -2,7 +2,7 @@
 
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 
-#if defined(__clang__) && __clang_major__ <= 13
+#if defined(__APPLE__) || (defined(__clang__) && __clang_major__ <= 13)
 #define clang_buggy_consteval constexpr
 #else
 #define clang_buggy_consteval consteval
