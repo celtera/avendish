f4096e01fd5a2deb87ab3f08b4cbf2367d66203a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-04-19 21:20:29 +0200

    [compiler] Support for GCC 13

--- include/avnd/common/function_reflection.hpp
+++ include/avnd/common/function_reflection.hpp
@@ -4,9 +4,16 @@
 
 #include <boost/mp11/algorithm.hpp>
 #include <boost/mp11/list.hpp>
+#include <boost/predef.h>
 
 #include <functional>
 #include <type_traits>
+
+#if defined(BOOST_COMP_GNUC)
+#if BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(13, 0, 0)
+#define AVND_NOEXCEPT_IN_TYPESYSTEM 1
+#endif
+#endif
 
 namespace avnd
 {
@@ -192,16 +199,164 @@ struct function_reflection<member>
   static constexpr const bool is_rvalue_reference = true;
 };
 
-template <typename R, typename... Args, R (*func)(Args...)>
-struct function_reflection<func>
+// Noexcept versions
+#if defined(AVND_NOEXCEPT_IN_TYPESYSTEM)
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) noexcept>
+struct function_reflection<member>
 {
   using arguments = boost::mp11::mp_list<Args...>;
   using return_type = R;
+  using class_type = T;
   static constexpr const auto count = sizeof...(Args);
-  static constexpr const bool is_noexcept = false;
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = false;
 };
 
-#if !defined(_MSC_VER)
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) const noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = true;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) volatile noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = true;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...)& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = true;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) && noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = true;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) const& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = true;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = true;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) const&& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = true;
+  static constexpr const bool is_volatile = false;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = true;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) volatile& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = true;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = true;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <typename T, typename R, typename... Args, R (T::*member)(Args...) volatile&& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = false;
+  static constexpr const bool is_volatile = true;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = true;
+};
+
+template <
+    typename T, typename R, typename... Args, R (T::*member)(Args...) const volatile& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = true;
+  static constexpr const bool is_volatile = true;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = true;
+  static constexpr const bool is_rvalue_reference = false;
+};
+
+template <
+    typename T, typename R, typename... Args, R (T::*member)(Args...) const volatile&& noexcept>
+struct function_reflection<member>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  using class_type = T;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_const = true;
+  static constexpr const bool is_volatile = true;
+  static constexpr const bool is_noexcept = true;
+  static constexpr const bool is_reference = false;
+  static constexpr const bool is_rvalue_reference = true;
+};
+
 template <typename R, typename... Args, R (*func)(Args...) noexcept>
 struct function_reflection<func>
 {
@@ -211,6 +366,15 @@ struct function_reflection<func>
   static constexpr const bool is_noexcept = true;
 };
 #endif
+
+template <typename R, typename... Args, R (*func)(Args...)>
+struct function_reflection<func>
+{
+  using arguments = boost::mp11::mp_list<Args...>;
+  using return_type = R;
+  static constexpr const auto count = sizeof...(Args);
+  static constexpr const bool is_noexcept = false;
+};
 
 template <auto F>
 using first_argument = boost::mp11::mp_first<typename function_reflection<F>::arguments>;
