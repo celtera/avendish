d149fc51ca5063ee1a473db02d076d03d880ed20
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-16 19:50:12 -0400

    [processes] Range mapper

---
+++ examples/Advanced/Utilities/RangeMapper.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <halp/controls.hpp>
+#include <halp/mappers.hpp>
+#include <halp/meta.hpp>
+#include <ossia/detail/math.hpp>
+
+namespace ao
+{
+
+struct RangeFilter
+{
+public:
+  halp_meta(name, "Range Mapper")
+  halp_meta(c_name, "range_mapper")
+  halp_meta(category, "Control/Mappings")
+  halp_meta(author, "Jean-Michaël Celerier")
+  halp_meta(description, "Map values in a numeric range")
+  halp_meta(uuid, "cb6f19b2-827d-420c-ad19-7a366f914026")
+
+  struct inputs_t
+  {
+    halp::val_port<"In", float> in;
+    halp::spinbox_f32<"In min", halp::range{-1e6, 1e6, 0.}> imin;
+    halp::spinbox_f32<"In max", halp::range{-1e6, 1e6, 1.}> imax;
+    halp::spinbox_f32<"Out min", halp::range{-1e6, 1e6, 0.}> omin;
+    halp::spinbox_f32<"Out max", halp::range{-1e6, 1e6, 1.}> omax;
+  } inputs;
+
+  struct
+  {
+    halp::val_port<"Out", float> out;
+  } outputs;
+
+  template <typename T>
+  static T
+  scale(T value, double src_min, double src_max, double dst_min, double dst_max) noexcept
+  {
+    const double sub = src_max - src_min;
+    if(sub == 0.)
+      return value;
+
+    auto ratio = (dst_max - dst_min) / sub;
+    value = dst_min + ratio * (value - src_min);
+    return value;
+  }
+
+  void operator()() noexcept
+  {
+    outputs.out = scale(inputs.in, inputs.imin, inputs.imax, inputs.omin, inputs.omax);
+  }
+};
+
+}
---
+++ examples/Advanced/Utilities/Trigger.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <halp/controls.hpp>
+#include <halp/mappers.hpp>
+#include <halp/meta.hpp>
+
+#include <optional>
+
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+namespace examples
+{
+struct Trigger
+{
+  halp_meta(name, "Trigger")
+  halp_meta(c_name, "avnd_trigger")
+  halp_meta(category, "Control/Mappings")
+  halp_meta(author, "Jean-Michaël Celerier")
+  halp_meta(description, "Send an impulse upon triggering")
+  halp_meta(uuid, "37998523-7a8a-4d12-8bd9-37e49adea083")
+
+  struct
+  {
+    halp::val_port<"Input", float> input;
+    halp::range_slider_f32<"ceil"> ceil;
+  } inputs;
+
+  struct
+  {
+    halp::val_port<"Enter", std::optional<halp::impulse>> enter;
+    halp::val_port<"Leave", std::optional<halp::impulse>> leave;
+  } outputs;
+
+  bool state{};
+  int64_t state_enter_frame;
+  int64_t state_change_frame;
+
+  using tick = halp::tick_musical;
+  void operator()(const halp::tick_musical& frames) noexcept
+  {
+    if(inputs.input.value > inputs.ceil.value.end && !state)
+    {
+      outputs.enter.value.emplace();
+      state = true;
+    }
+    else if(inputs.input.value < inputs.ceil.value.start && state)
+    {
+      outputs.leave.value.emplace();
+      state = false;
+    }
+  }
+};
+}
