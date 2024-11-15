f2900dc9b3e05c8bb58e48e055e275176f548253
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-15 16:33:46 -0500

    [tests] Add basic catch2 test infrastructure

---
+++ tests/objects/gain.cpp
@@ -0,0 +1,17 @@
+#include <catch2/catch_all.hpp>
+
+#include <examples/Advanced/Utilities/Gain.hpp>
+TEST_CASE("Gain test", "[gain]")
+{
+  ao::Gain gain;
+  GIVEN("Correct input")
+  {
+    ao::Gain::inputs inputs;
+    inputs.gain.value = 0.5f;
+    THEN("Correct output")
+    {
+        double res = gain(1.5, inputs);
+        REQUIRE(res == 1.5 * 0.5);
+    }
+  }
+}
