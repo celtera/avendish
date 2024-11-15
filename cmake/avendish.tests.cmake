e6b10ce2a77ac09d744c24d0e20095aa581207d8
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-15 16:33:46 -0500

    [tests] Add basic catch2 test infrastructure

--- cmake/avendish.tests.cmake
+++ cmake/avendish.tests.cmake
@@ -10,7 +10,28 @@ function(avnd_add_executable_test theTarget theFile)
   avnd_common_setup("" "${theTarget}")
 endfunction()
 
+function(avnd_add_catch_test theTarget theFile)
+  add_executable("${theTarget}" "${theFile}")
+  avnd_common_setup("" "${theTarget}")
+  target_link_libraries("${theTarget}" PRIVATE Catch2::Catch2WithMain)
+endfunction()
+
 if(BUILD_TESTING)
+  if(NOT TARGET Catch2::Catch2WithMain)
+    FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v3.4.0
+    )
+    
+    FetchContent_MakeAvailable(Catch2)
+    if(NOT TARGET Catch2::Catch2WithMain)
+        message(WARNING "libremidi: Catch2::Catch2WithMain target not found")
+        return()
+    endif()
+  endif()
+
+
   avnd_add_static_test(test_introspection tests/test_introspection.cpp)
   avnd_add_static_test(test_introspection_rec tests/test_introspection_rec.cpp)
   avnd_add_static_test(test_predicate tests/test_predicate.cpp)
@@ -21,4 +42,7 @@ if(BUILD_TESTING)
   avnd_add_static_test(test_index_in_struct tests/test_index_in_struct.cpp)
 
   avnd_add_executable_test(test_introspection_many tests/test_introspection_many.cpp)
+
+  avnd_add_catch_test(test_gain tests/objects/gain.cpp)
 endif()
+
