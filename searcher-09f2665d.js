bb3a8c97465f2f049b764244fe61a0ec2d24dd47
jcelerier <jcelerier@users.noreply.github.com>
2026-09-12 18:46:36 +0000

    deploy: 8b3391296afa0e21487693efc7283538091c5306

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c29f2a46.js',
+                path_to_root + 'searchindex-4f8c5fee.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
